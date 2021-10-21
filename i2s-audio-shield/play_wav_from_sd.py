0582650d599826200a7bb941a62b4ef873d1aa34
Mike Causer <mcauser@gmail.com>
2021-10-21 12:12:12 +1100

    New example wavs

--- i2s-audio-shield/play_wav_from_sd.py
+++ i2s-audio-shield/play_wav_from_sd.py
@@ -11,40 +11,60 @@
 # machine.I2S docs:
 # https://docs.micropython.org/en/latest/library/machine.I2S.html
 
+# Free tool to convert mp3 to wav:
+# https://audio.online-convert.com/convert-to-wav
+
+# Drum loop from Zapsplat.com:
+# https://www.zapsplat.com/sound-effect-category/loops/
+
+# Setup:
+# copy wavplayer.py to your filesystem
+# copy .wav files to a SD card
+
 import os
 import time
 from machine import Pin
 from wavplayer import WavPlayer
 
-# configure SD card
+# configure and mount SD card
 from machine import SDCard
 sd = SDCard(slot=2) # sck=18, mosi=23, miso=19, cs=5
 os.mount(sd, "/sd")
 
-# copy the quack.wav file to your SD card
+# list files to your SD card
 os.listdir('/sd')
-# ['quack.wav']
+# ['fart.wav', 'drum_loop_1.wav', 'drum_loop_3.wav', 'drum_loop_3.wav']
 
 # adjust the gain (volume)
-gain = Pin(14)
-gain.init(Pin.IN, pull=Pin.PULL_DOWN) # gain 5: louder
-gain.init(Pin.OUT, value=0)           # gain 4: loud
-gain.init(Pin.IN, pull=None)          # gain 3: middle
-gain.init(Pin.OUT, value=1)           # gain 2: quiet
-gain.init(Pin.IN, pull=Pin.PULL_UP)   # gain 1: quieter
+# gain = Pin(14)
+# gain.init(Pin.IN, pull=Pin.PULL_DOWN) # gain 5: louder
+# gain.init(Pin.OUT, value=0)           # gain 4: loud
+# gain.init(Pin.IN, pull=None)          # gain 3: middle
+# gain.init(Pin.OUT, value=1)           # gain 2: quiet
+# gain.init(Pin.IN, pull=Pin.PULL_UP)   # gain 1: quieter
 
 # init WavPlayer
 wp = WavPlayer(id=0, sck_pin=Pin(27), ws_pin=Pin(26), sd_pin=Pin(25), ibuf=40000)
 
 # play once
-wp.play("quack.wav", loop=False)
+wp.play("fart.wav", loop=False)
 
-# play forever
-wp.play("quack.wav", loop=True)
+# loop forever
+wp.play("drum_loop_1.wav", loop=True)
+# wp.play("drum_loop_2.wav", loop=True)
+# wp.play("drum_loop_3.wav", loop=True)
 
 if wp.isplaying():
-    print('yes quack.wav is playing')
+    print('yes drum loop is playing')
 
+# pause the drum loop
 wp.pause()
+
+# resume from where it was paused
 wp.resume()
+
+# stop playing
 wp.stop()
+
+# eject SD card
+# os.umount("/sd")
